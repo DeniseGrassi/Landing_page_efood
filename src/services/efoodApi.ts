72b7fbf95c549476cc02c0652204437560751f90
Denise Grassi <denygrassi_@hotmail.com>
2025-11-25 22:13:55 -0300

    consertando erro 404

--- src/services/efoodApi.ts
+++ src/services/efoodApi.ts
@@ -27,18 +27,18 @@ export type CheckoutResponse = { orderId: string }
 export const efoodApi = createApi({
   reducerPath: 'efoodApi',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://api-ebac.vercel.app/api/efood/restaurantes'
+    baseUrl: 'https://api-ebac.vercel.app/api/efood'
   }),
   endpoints: (builder) => ({
     getRestaurantes: builder.query<Restaurante[], void>({
-      query: () => 'restaurantes'
+      query: () => '/restaurantes'
     }),
     getRestauranteById: builder.query<Restaurante, string | number>({
-      query: (id) => `restaurantes/${id}`
+      query: (id) => `/restaurantes/${id}`
     }),
     checkout: builder.mutation<CheckoutResponse, CheckoutPayload>({
       query: (body) => ({
-        url: 'checkout',
+        url: '/checkout',
         method: 'POST',
         body
       })
